545b75ffae1c24f1087889e9343ee98edef37409
guts <wiliam.tae@gmail.com>
2021-04-18 12:24:57 -0300

    feat: save user on db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,21 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+    
+      try {
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
