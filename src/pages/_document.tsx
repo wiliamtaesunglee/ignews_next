506782a649a49072c59d388b4f8931d36ced1a7a
guts <wiliam.tae@gmail.com>
2021-04-18 11:09:51 -0300

    fix: remove title in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
           <link rel='shortcut icon' href='/favicon.png' type='image/png' />
-          <title>igNews</title>
         </Head>
         <body>
           <Main />
