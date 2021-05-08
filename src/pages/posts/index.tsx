2272d6ca774fd5a3ac0ce2416110211b3977f5f3
guts <wiliam.tae@gmail.com>
2021-05-08 08:38:31 -0300

    feat: create posts list page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,21 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 import styles from './styles.module.scss'
 
-const Posts = () => {
+type Post = {
+  slug: string;
+  title: string;
+  exerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+const Posts = ({ posts }: PostsProps) => {
   return (
     <>
       <Head>
@@ -13,21 +25,13 @@ const Posts = () => {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In his guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In his guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In his guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} >
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.exerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -46,9 +50,21 @@ export const getStaticProps: GetStaticProps = async () => {
   )
 
   console.log('>>', response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      exerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: { posts }
   }
 }
 
